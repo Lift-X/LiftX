2e11f163b880b3539457d7c681dcadacb15bff14
Zusier <zusier@disroot.org>
2022-05-08 21:36:35 -0500

    No use for this enum currently

--- src/equipment.rs
+++ src/equipment.rs
@@ -22,11 +22,6 @@ pub const POUNDS: WeightType = WeightType {
     short_name: "lbs",
 };
 
-enum WeightTypeVariant {
-    Kilograms,
-    Pounds,
-}
-
 /* deserializable *kind of*
 lazy_static!(
     #[derive(Serialize, Deserialize, PartialEq, Debug, Clone, Copy)]
