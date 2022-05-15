8d51faece1f122b7c42e97a6f517123ea4f6ccf4
Zusier <zusier@disroot.org>
2022-05-15 10:15:40 -0500

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,60 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
+
+function serve() {
+	let server;
+
+	function toExit() {
+		if (server) server.kill(0);
+	}
+
+	return {
+		writeBundle() {
+			if (server) return;
+			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+				stdio: ['ignore', 'inherit', 'inherit'],
+				shell: true
+			});
+
+			process.on('SIGTERM', toExit);
+			process.on('exit', toExit);
+		}
+	};
+}
 
 export default {
 	input: 'client/main.js',
 	output: {
 		sourcemap: true,
 		format: 'iife',
-		name: 'wlrs',
+		name: 'app',
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
 		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			hydratable: true,
-
-			onwarn: (warning, handler) => {
-				const { code, frame } = warning;
-				if (code === "css-unused-selector") return;
-				handler(warning);
-			},
-			// we'll extract any component CSS out into
-			// a separate file — better for performance
-			css: css => {
-				css.write('bundle.css');
-			},
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production
+			}
 		}),
+		// we'll extract any component CSS out into
+		// a separate file - better for performance
+		css({ output: 'bundle.css' }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration —
+		// some cases you'll need additional configuration -
 		// consult the documentation for details:
-		// https://github.com/rollup/rollup-plugin-commonjs
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
-			dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
+			dedupe: ['svelte']
 		}),
 		commonjs(),
 
@@ -59,21 +74,3 @@ export default {
 		clearScreen: false
 	}
 };
-
-function serve() {
-	let started = false;
-
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
-				// replaced last argument from --dev - try watch next
-				// require('child_process').spawn('npm', ['run', 'start', '', ''],
-				// {
-				// 	stdio: ['ignore', 'inherit', 'inherit'],
-				// 	shell: true
-				// });
-			}
-		}
-	};
-}
