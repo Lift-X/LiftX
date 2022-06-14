87eb891da0e623cdd098e9e6953a0aeacd61f2d3
Zusier <zusier@disroot.org>
2022-06-13 22:49:02 -0500

    Simplify navbar

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 #![forbid(unsafe_code)]
 // Use all clippy lints (just to get an idea)
 #![warn(clippy::all, clippy::pedantic)]
+#![deny(
+    bad_style,
+    const_err,
+    dead_code,
+    improper_ctypes,
+    non_shorthand_field_patterns,
+    no_mangle_generic_items,
+    overflowing_literals,
+    path_statements,
+    patterns_in_fns_without_body,
+    private_in_public,
+    unconditional_recursion,
+    unused_allocation,
+    unused_comparisons,
+    unused_parens,
+    while_true
+)]
 #[macro_use]
 extern crate rocket;
 mod api;
