b2d2a3de708af120401fead4c55066607572a515
Zusier <zusier@disroot.org>
2022-07-06 23:14:28 -0500

    Start settings..

---
+++ client/routes/settings.svelte
@@ -0,0 +1,6 @@
+
+<div class="separator">
+    <h1 class="font-bold">Settings</h1>
+    <hr>
+    <label>Show Exercise Graph: <input type="checkbox" class="toggle border-primary-500 bg-primary-500" /></label>
+</div>
