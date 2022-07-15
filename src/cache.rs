719b3122aaed49a61496fecbeafc10e72744e3af
Zusier <zusier@disroot.org>
2022-07-14 21:38:52 -0500

    Update docs

--- src/cache.rs
+++ src/cache.rs
@@ -1,14 +1,13 @@
 use rocket::response;
 /// Allows for a `Cache-Control` header to be used on any response that implements `Responder`.
-/// The `cache_control` field is
+/// <https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control>.
+/// `cache_control` should be **just** the value of the `Cache-Control` header. "Cache-Control:" should not be included.
 pub struct CacheableResponse<T> {
     pub data: T,
     pub cache_control: String,
 }
 
-impl<'r, 'o: 'r, T: response::Responder<'r, 'o>> response::Responder<'r, 'o>
-    for CacheableResponse<T>
-{
+impl<'r, 'o: 'r, T: response::Responder<'r, 'o>> response::Responder<'r, 'o> for CacheableResponse<T> {
     fn respond_to(self, req: &'r rocket::Request<'_>) -> response::Result<'o> {
         response::Response::build_from(self.data.respond_to(req)?)
             .raw_header("Cache-Control", self.cache_control)
